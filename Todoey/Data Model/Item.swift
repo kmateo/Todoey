9d0444ae49e5ce87ac69cb1959e5fb91284c3e4d
Kevin Mateo Garcia <kevin_mateo5@hotmail.com>
2018-11-30 16:34:09 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by kevin Mateo on 30/11/2018.
+//  Copyright © 2018 Kevin Mateo. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+}
